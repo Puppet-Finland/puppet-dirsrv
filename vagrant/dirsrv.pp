9b9fbdb90dc3a992c31be2f5691d0005fddf9c38
Samuli Seppänen <samuli@openvpn.net>
2018-02-15 14:19:00 +0200

    Vagrant: call the main class with reasonable test parameters
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- vagrant/dirsrv.pp
+++ vagrant/dirsrv.pp
@@ -1,5 +1,12 @@
 # This manifest is only used by Vagrant
 
+$servermonitor = 'root@localhost'
+
 class { '::dirsrv':
-    manage => false,
+    serveridentifier           => 'dirsrv',
+    suffix                     => 'dc=example,dc=org',
+    rootdn_pwd                 => 'vagrant',
+    server_admin_pwd           => 'vagrant',
+    config_directory_admin_pwd => 'vagrant',
+    admin_bind_ip              => '0.0.0.0',
 }
