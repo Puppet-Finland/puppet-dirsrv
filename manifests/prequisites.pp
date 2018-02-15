aaf3cd5fd338823cab97cd852ee9efffc0146ca6
Samuli Seppänen <samuli@openvpn.net>
2018-02-15 15:53:04 +0200

    Work around issue in datacentred-ldap
    Datacentred-ldap assumes the net-ldap rubygem is installed using Puppetlabs'
    gem.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/prequisites.pp
+++ manifests/prequisites.pp
@@ -5,10 +5,16 @@
 #
 class dirsrv::prequisites inherits dirsrv::params {
 
-    # Ruby-net-ldap is not required by 389ds. However, the ldap_entry puppet 
-    # type included in the datacentred-ldap module will refuse to work if 
-    # ruby-net-ldap is missing. Because ldap_entry is used to configure 389ds it 
-    # needs to work. The end result: we must install ruby-net-ldap.
+    # The net-ldap rubygem is not required by 389ds. However, the ldap_entry 
+    # puppet type included in the datacentred-ldap module will refuse to work if 
+    # net-ldap is missing. Because ldap_entry is used to configure 389ds it 
+    # needs to work. The end result: we must install the net-ldap gem. In 
+    # addition net-ldap needs to be installed using the gem bundled with 
+    # puppet-agent.
     include ::ruby
-    include ::ruby::net_ldap
+
+    package { 'net-ldap':
+        ensure   => 'present',
+        provider => 'puppet_gem',
+    }
 }
