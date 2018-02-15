300839fb8926aecac36e858a14d78001bb69ab35
Samuli Seppänen <samuli@openvpn.net>
2018-02-15 14:49:55 +0200

    Vagrant: enable monit and packetfilter by default
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- vagrant/dirsrv.pp
+++ vagrant/dirsrv.pp
@@ -2,11 +2,20 @@
 
 $servermonitor = 'root@localhost'
 
+include ::packetfilter::endpoint
+include ::monit
+
 class { '::dirsrv':
-    serveridentifier           => 'dirsrv',
-    suffix                     => 'dc=example,dc=org',
-    rootdn_pwd                 => 'vagrant',
-    server_admin_pwd           => 'vagrant',
-    config_directory_admin_pwd => 'vagrant',
-    admin_bind_ip              => '0.0.0.0',
+    manage_monit                 => true,
+    manage_packetfilter          => true,
+    serveridentifier             => 'dirsrv',
+    suffix                       => 'dc=example,dc=org',
+    rootdn_pwd                   => 'vagrant',
+    server_admin_pwd             => 'vagrant',
+    config_directory_admin_pwd   => 'vagrant',
+    admin_bind_ip                => '0.0.0.0',
+    dirsrv_allow_ipv4_address    => '0.0.0.0',
+    dirsrv_allow_ipv6_address    => '::1',
+    admin_srv_allow_ipv4_address => '0.0.0.0',
+    admin_srv_allow_ipv6_address => '::1',
 }
